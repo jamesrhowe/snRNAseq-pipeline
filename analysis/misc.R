426b756f6e3acfcf518f3c6dd153e38a641cb396
jamesrhowe <j3howe@ucsd.edu>
2020-09-20 13:06:47 -0700

    Changed some helper functions to make more versatile, changed calls to match. Also added in a high outlier filter and streamlined mitochondrial/ribosomal filtration.

--- analysis/misc.R
+++ analysis/misc.R
@@ -1,11 +1,11 @@
 # contains helper functions for analysis notebooks
 
 # this used to collect summary stats for every dataset
-count_all <- function(step){
-  cat(count_stats(paste("counts per", step), array@meta.data$nCount_RNA),
-      count_stats(paste("features per", step), array@meta.data$nFeature_RNA),
-      count_stats(paste("mitochondrial proportion per", step), array@meta.data$Mito_proportion),
-      count_stats(paste("ribosomal proportion per", step), array@meta.data$Ribo_proportion), sep = "\n")
+count_all <- function(step, object){
+  cat(count_stats(paste("counts per", step), object@meta.data$nCount_RNA),
+      count_stats(paste("features per", step), object@meta.data$nFeature_RNA),
+      count_stats(paste("mitochondrial proportion per", step), object@meta.data$Mito_proportion),
+      count_stats(paste("ribosomal proportion per", step), object@meta.data$Ribo_proportion), sep = "\n")
 }
 
 count_stats <- function(x, y){
